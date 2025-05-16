1762f1357cf3c2f45013e5c7c755918f2d7c6530
shaxzod999 <shaxzodumarov2009@gmail.com>
2025-05-17 03:35:32 +0500

    Button

--- src/page/AppRouter.jsx
+++ src/page/AppRouter.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import AddProduct from "./AddProduct";
